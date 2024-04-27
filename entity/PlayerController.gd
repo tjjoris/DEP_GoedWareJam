2db09a3102c85568a53dc295a3990e893620b24e
Yan Uchitel <yan.uchitel@gmail.com>
2024-04-26 23:02:13 -0400

    added double jump
    added a double jump and the ability to jump after falling from a ledge

--- entity/PlayerController.gd
+++ entity/PlayerController.gd
@@ -1,27 +1,32 @@
 extends CharacterBody2D
 
-
+# Exported movement variables
 @export var SPEED: float = 250.0
 @export var ACCELERATION: float = 1700.0
 @export var FRICTION: float = 1400.0
 
+# Exporrted jump related variables
 @export var JUMP_VELOCITY: float = -520.0
 @export var MAX_JUMPS: int = 2
 @export var GRAVITY: float = 1500.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+# Internal jump related variables
+var jumps_remaining: int = MAX_JUMPS
+var has_jumped: bool = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Handles gravity.
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
-		
+		# A possible way to limit the fall speed to not scale forever
+		# clampf(velocity.y, JUMP_VELOCITY, GRAVITY)
+	else:
+		jumps_remaining = MAX_JUMPS
+		has_jumped = false
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Handles jump.
+	if Input.is_action_just_pressed("jump"):# and is_on_floor():
+		handle_jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction: float = Input.get_axis("left", "right")	
@@ -31,3 +36,15 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0.0, FRICTION * delta)
 
 	move_and_slide()
+
+# Handles double jump
+func handle_jump() -> void:
+	# Allows you to jump after falling from ledge
+	if is_on_floor() == false and has_jumped == false:
+		jumps_remaining -= 1
+	
+	# counts the amount of jumps remaining
+	if jumps_remaining > 0:
+		has_jumped = true
+		jumps_remaining -= 1
+		velocity.y = JUMP_VELOCITY
