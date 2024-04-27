c7ebd52d464fa50b26aeec2be2b777c397c5b747
Yan Uchitel <yan.uchitel@gmail.com>
2024-04-26 23:55:53 -0400

    made he jump feel better
    sped up the fall time relatively to the time the player spent in the air

--- entity/PlayerController.gd
+++ entity/PlayerController.gd
@@ -1,32 +1,39 @@
 extends CharacterBody2D
 
 # Exported movement variables
-@export var SPEED: float = 250.0
+@export var SPEED: float = 220.0
 @export var ACCELERATION: float = 1700.0
 @export var FRICTION: float = 1400.0
 
 # Exporrted jump related variables
 @export var JUMP_VELOCITY: float = -520.0
 @export var MAX_JUMPS: int = 2
-@export var GRAVITY: float = 1500.0
+@export var GRAVITY: float = 1600.0
 
 # Internal jump related variables
 var jumps_remaining: int = MAX_JUMPS
 var has_jumped: bool = false
 
+var air_time = 0.0
+const BONUS_GRAVITY = 2.0
+
 func _physics_process(delta: float) -> void:
 	# Handles gravity.
 	if not is_on_floor():
-		velocity.y += GRAVITY * delta
+		air_time = 0.0
 		# A possible way to limit the fall speed to not scale forever
 		# clampf(velocity.y, JUMP_VELOCITY, GRAVITY)
 	else:
+		air_time += delta
 		jumps_remaining = MAX_JUMPS
 		has_jumped = false
+	# Speeds up the players fall depending on how long they were in the air
+	velocity.y += (GRAVITY + GRAVITY * air_time * BONUS_GRAVITY) * delta
+
 
 	# Handles jump.
 	if Input.is_action_just_pressed("jump"):# and is_on_floor():
-		handle_jump()
+		handle_jump(delta)
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction: float = Input.get_axis("left", "right")	
@@ -37,12 +44,13 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+
 # Handles double jump
-func handle_jump() -> void:
+func handle_jump(delta: float) -> void:
 	# Allows you to jump after falling from ledge
 	if is_on_floor() == false and has_jumped == false:
 		jumps_remaining -= 1
-	
+
 	# counts the amount of jumps remaining
 	if jumps_remaining > 0:
 		has_jumped = true
