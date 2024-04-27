1144b0733bc24684b210001d94a4bd85e38f3303
Yan Uchitel <yan.uchitel@gmail.com>
2024-04-26 22:19:28 -0400

    added friction acceleration and exported gravity variable

--- entity/PlayerController.gd
+++ entity/PlayerController.gd
@@ -1,17 +1,22 @@
 extends CharacterBody2D
 
 
-@export var SPEED: float = 300.0
-@export var JUMP_VELOCITY: float = -500.0
+@export var SPEED: float = 250.0
+@export var ACCELERATION: float = 1700.0
+@export var FRICTION: float = 1400.0
+
+@export var JUMP_VELOCITY: float = -520.0
+@export var MAX_JUMPS: int = 2
+@export var GRAVITY: float = 1500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+#var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += GRAVITY * delta
 		
 
 	# Handle jump.
@@ -19,11 +24,10 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
+	var direction: float = Input.get_axis("left", "right")	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0.0, FRICTION * delta)
 
 	move_and_slide()
