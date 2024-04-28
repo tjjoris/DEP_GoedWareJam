880604fd8f1fc606eece41feda411cbc1d7b9a8b
tjjoris <tjjoris@gmail.com>
2024-04-28 00:18:07 -0400

    Update level_loader.gd

--- levels/level_loader.gd
+++ levels/level_loader.gd
@@ -1,13 +1,13 @@
-extends Node
-
-var next_scene: PackedScene = preload("res://levels/_level_blueprint.tscn")
-
-
-func _my_level_was_completed():
-	print(next_scene)
-	#sets next level to load to level_luke2
-	get_tree().change_scene_to_file("res://levels/level_luke2.tscn")
-
-
-func _on_load_level_2_button_pressed():
-	_my_level_was_completed()
+#extends Node
+#
+#var next_scene: PackedScene = preload("res://levels/_level_blueprint.tscn")
+#
+#
+#func _my_level_was_completed():
+	#print(next_scene)
+	##sets next level to load to level_luke2
+	#get_tree().change_scene_to_file("res://levels/level_luke2.tscn")
+#
+#
+#func _on_load_level_2_button_pressed():
+	#_my_level_was_completed()
