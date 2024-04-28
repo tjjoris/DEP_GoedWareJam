fe11fca769c26d7f247b53340df944a4334f5cf6
tjjoris <tjjoris@gmail.com>
2024-04-28 18:08:43 -0400

    level loader 2 working on button press

---
+++ levels/NextLevelButton.gd
@@ -0,0 +1,15 @@
+extends Button
+
+func _on_button_pressed():
+	LevelLoader2.goto_scene("res://levels/level_luke2.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
