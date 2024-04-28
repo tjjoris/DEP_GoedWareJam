74f82cced2e17eef88dcb789c284c900d847173a
AndrewThinhNguyen <andrewnguyen2812@gmail.com>
2024-04-28 12:52:24 -0400

    TriggerObject

---
+++ levels/detects_collisions.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+func _ready():
+	# Đăng ký signal.
+	connect("body_entered", self, "_on_body_entered")
+
+func _on_body_entered(body):
+	# In ra thông báo khi một vật thể va chạm.
+	print("Một vật thể đã va chạm!")
