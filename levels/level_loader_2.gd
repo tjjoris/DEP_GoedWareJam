ffc41e6a2cf14e122161c0f422b70a0e0c1acc49
tjjoris <tjjoris@gmail.com>
2024-04-28 13:03:39 -0400

    old level loader gone and new level loader in.
    seems to be working.
    next to make it a singleton.

--- levels/level_loader_2.gd
+++ levels/level_loader_2.gd
@@ -1,13 +1,18 @@
 extends Node
 
-var next_scene: PackedScene = preload("res://levels/_level_blueprint.tscn")
+#var next_scene: PackedScene = preload("res://levels/_level_blueprint.tscn")
 
 
 func _my_level_was_completed():
-	print(next_scene)
+	#print(next_scene)
 	#sets next level to load to level_luke2
 	get_tree().change_scene_to_file("res://levels/level_luke2.tscn")
 
 
 func _on_load_level_2_button_pressed():
 	_my_level_was_completed()
+
+
+func _on_button_pressed():
+	_my_level_was_completed()
+	pass # Replace with function body.
