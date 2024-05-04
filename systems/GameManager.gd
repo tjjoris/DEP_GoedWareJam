ad8b06112a869d76b8770ccc86eab503d88c1973
Tars1n <mudd.rosal@gmail.com>
2024-05-04 17:04:38 -0400

    Pause menu started

--- systems/GameManager.gd
+++ systems/GameManager.gd
@@ -4,14 +4,16 @@ extends Node
 #warning-ignore:unused_signal
 
 signal collectable_value_changed(_current_level_collectables)
+#signal toggle_game_paused(is_paused : bool)
 
 static var is_shifted: bool = false
 var total_collectables: int = 0
 static var player_can_move: bool = true
 static var player_is_dead: bool = false
 static var player_can_phase: bool = true
-
+static var dialogue_canvas: CanvasLayer
 var _current_level_collectables = 0
+
 var collectable_score: int:
 	get:
 		return _current_level_collectables
@@ -19,10 +21,24 @@ var collectable_score: int:
 		_current_level_collectables = value
 		collectable_value_changed.emit(_current_level_collectables)
 
+#var game_paused : bool = false:
+	#get:
+		#return game_paused
+	#set(value):
+		#print("game paused request: " + str(value))
+		#game_paused = value
+		#get_tree().paused = game_paused
+		#emit_signal("toggle_game_paused", game_paused)
+
+
 func _ready() -> void:
 	total_collectables = 0
 
-static var dialogue_canvas: CanvasLayer
+
+#func _input(event : InputEvent):
+	#if(event.is_action_pressed("ui_cancel")):
+		#game_paused = !game_paused
+
 
 func level_completed():
 	total_collectables += _current_level_collectables
---
+++ systems/pause_menu.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	hide()
+	#GameManager.connect("toggle_game_paused", _on_game_manager_toggle_game_paused)
+
+func _input(event : InputEvent):
+	if(event.is_action_pressed("ui_cancel")):
+		get_tree().paused = not get_tree().paused
+		if get_tree().paused:
+			show()
+		else:
+			hide()
+
+func _on_restart_button_pressed() -> void:
+	LevelLoader.reload_current_scene()
+
+
+func _on_main_menu_button_pressed() -> void:
+	LevelLoader.load_scene(0)
+
+
+#func _on_game_manager_toggle_game_paused(is_paused : bool):
+	#if is_paused:
+		#show()
+	#else:
+		#hide()
+
+
+func _on_resume_button_pressed() -> void:
+	get_tree().paused = false
+	hide()
+
