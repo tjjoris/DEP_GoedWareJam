230df13571e54ce6acbf41d6d14c6ddb0cc3e18c
Tars1n <mudd.rosal@gmail.com>
2024-05-04 01:38:37 -0400

    Fixed tileset bug, created CollectableSample object

--- systems/TriggerObject2D.gd
+++ systems/TriggerObject2D.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+@export var destroyed_on_contact = true
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		print("Player has entered the trigger area!")
+		print("Player has collected a sample!")
+		queue_free()
