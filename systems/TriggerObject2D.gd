cc58f877495d2ab07b420529990271d3d53b8b8f
Tars1n <mudd.rosal@gmail.com>
2024-05-05 12:44:40 -0400

    Pickup Collectable working + many sfx

--- systems/TriggerObject2D.gd
+++ systems/TriggerObject2D.gd
@@ -5,8 +5,6 @@ extends Area2D
 @onready var sprite_2d: Sprite2D = %Sprite2D
 
 @onready var collision_shape_2d: CollisionShape2D = %CollisionShape2D
-@onready var audio_player: AudioStreamPlayer = %AudioStreamPlayer
-const clip = preload("res://audio/sfx/Sample_00.wav")
 
 func _ready() -> void:
 	handle_phase()
@@ -14,11 +12,7 @@ func _ready() -> void:
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		if audio_player and clip:
-			print("should play noise here")
-			audio_player.stream = clip
-			audio_player.play()
-		GameManager.collectable_score += 1
+		GameManager.picked_up_collectable_sample()
 		remove_object()
 
 
