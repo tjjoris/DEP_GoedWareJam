be83e65069198e3496d825b970bc60459b8a89cc
Tars1n <mudd.rosal@gmail.com>
2024-05-13 15:59:40 -0400

    text tweaks, removed cruft code

--- entity/jump_handler.gd
+++ entity/jump_handler.gd
@@ -16,8 +16,6 @@ extends Node2D
 ## Height of jump when jump button is held. 
 ## Amount of negative gravity added while holding jump.
 @export var HELD_JUMP_HEIGHT = 8
-var _was_on_floor: bool
-
 
 # Internal jump related variables
 var jumps_remaining: int = MAX_JUMPS
@@ -43,11 +41,6 @@ func handle_jump(entity: CharacterBody2D) -> void:
 		entity.velocity.y = JUMP_VELOCITY
 		audio_player.play()
 	#print(jumps_remaining)
-
-
-func is_coyote_time() -> bool:
-	var is_coyote_time = false
-	return is_coyote_time
 
 
 func start_coyote_timer():
